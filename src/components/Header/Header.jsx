b45cdd693eef80c889f2392ee1302837ef375c2e
confidential-nt <youyoy689@gmail.com>
2023-09-23 14:07:23 +0900

    [feat] 모든 모달을 감싸는 parent modal 구현

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,15 +3,28 @@ import cn from 'classnames';
 import { Button, Typography } from '@goorm-dev/gds-challenge';
 
 import styles from './Header.module.scss';
+import ParentModal from '../Modals/ParentModal/ParentModal';
+import { useState } from 'react';
 
 const Header = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const toggle = () => {
+		setIsOpen((prev) => {
+			return !prev;
+		});
+	};
 	return (
 		<header className={cn(styles.header)}>
 			<div className={cn(styles.contents)}>
 				<Typography token="h5">
 					구름톤 챌린지 참여자 정보 수집
 				</Typography>
-				<Button size="lg">설문조사 참여하기</Button>
+				<Button size="lg" onClick={toggle}>
+					설문조사 참여하기
+				</Button>
+				<ParentModal isOpen={isOpen} toggle={toggle}>
+					<p>{123}</p>
+				</ParentModal>
 			</div>
 		</header>
 	);
