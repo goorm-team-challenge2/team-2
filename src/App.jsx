3cfd51024e2bd130a5ca1b6d1734971aefeccddc
GHeeJeon <jhjeon_99@naver.com>
2023-09-23 14:08:18 +0900

    [jihee] update empty view
    중앙정렬!

--- src/App.jsx
+++ src/App.jsx
@@ -14,3 +14,6 @@ function App() {
 }
 
 export default App;
+
+// https://www.figma.com/file/ihvdLEzT5Q8ivyJjnOv2h0/%EC%98%A4%ED%94%84%EB%9D%BC%EC%9D%B8-%ED%8C%80-%EC%B1%8C%EB%A6%B0%EC%A7%80-%EA%B3%BC%EC%A0%9C?type=design&node-id=0-1&mode=design&t=b3yT8aE2EwWvlT8h-0
+
